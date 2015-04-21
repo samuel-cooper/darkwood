efe504eea2de0e724ca1894bca1d4bd6cbd78654
Samuel Cooper <samuel.cooper03@gmail.com>
2015-04-21 13:16:45 +1000

    Basic battle completed, needs testing

---
+++ src/Fighter.java
@@ -0,0 +1,50 @@
+public class Fighter {
+	String name;
+	int level;
+	double hp;
+	double atk;
+	double ac;
+	double exp;
+	Equip weapon;
+	Equip armor;
+	
+	String getName() {
+		return name;
+	}
+	
+	double getHP() {
+		return hp;
+	}
+	
+	void setHP(double a) {
+		hp = a;
+	}
+	
+	double getATK() {
+		return atk;
+	}
+	
+	void setATK(double a) {
+		atk = a;
+	}
+	
+	double getAC() {
+		return ac;
+	}
+	
+	void setAC(double a) {
+		ac = a;
+	}
+
+	double getEXP() {
+		return exp;
+	}
+	
+	void addEXP(double a) {
+		exp += a;
+	}
+	
+	void setLVL(int a) {
+		level = a;
+	}
+}
